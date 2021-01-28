251c631a00c8f2681a042f3559803080731718a5
Adrian <adrianfdez469@gmail.com>
2021-01-28 10:35:42 -0500

    Styling demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Typography, Icon } from '@material-ui/core';
 import InputSelect from 'react-material-selectable-inputtext';
+
+
 
 const App = () => {
   const [allCountries, setAllCountries] = React.useState([]);
-  const [list, setList] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
   React.useEffect(() => {
     (async () => {
       const countriesResp = await fetch('https://restcountries.eu/rest/v2/all');
       if (countriesResp.ok) {
         const countries = await countriesResp.json();
+        console.log(countries);
         const mappedCountries = countries.map(c => {
-          return { id: c.name, text: c.name, population: c.population }
+          // The object passed to the optionsList prop and excludedOptions must have id and text properties
+          return { id: c.name, text: c.name, population: c.population, flag: c.flag }
         });
         setAllCountries(mappedCountries);
       }
@@ -18,25 +23,36 @@ const App = () => {
   }, []);
 
   const onAddHandler = (item) => {
-    setList([...list, item]);
+    setSelected([...selected, item]);
   }
 
   return (
     <div style={{ margin: '20px', fontSize: '20px' }}>
-      <h2>To see what the component can do, follow the steps.</h2>
+      <Typography variant="h3">To see what the component can do, follow the steps.</Typography>
       <ol>
-        <li>Focus the component and white an "A" letter, it will pop up all the countries that begin with "A"</li>
-        <li>Press the arrow down key from your keyboard, and choose "Albania", then press "Enter"</li>
-        <li>Again write an "A" letter, you'll see that "Albania" is no longer available. This time with your mouse select other country option and press "Enter" or press the plus button</li>
-        <li>Now write anther country name without selecting it from the list and added, you can see after that the country is no logner available on the list</li>
-        <li>Also you can write anything that is not on the list, and added as well</li>
-        <li>Be aware that we you write a substring, you can navigate throgth the options with down arrow key, but you can comeback to the input with the ip arrow key</li>
+        <li><Typography variant="body1">Focus the component and white an "A" letter, it will pop up all the countries that begin with "A"</Typography></li>
+        <li><Typography variant="body1">Press the arrow down key from your keyboard, and choose "Albania", then press "Enter"</Typography></li>
+        <li><Typography variant="body1">Again write an "A" letter, you'll see that "Albania" is no longer available. This time with your mouse select other country option and press "Enter" or press the plus button</Typography></li>
+        <li><Typography variant="body1">Now write anther country name without selecting it from the list and added, you can see after that the country is no logner available on the list</Typography></li>
+        <li><Typography variant="body1">Also you can write anything that is not on the list, and added as well</Typography></li>
+        <li><Typography variant="body1">Be aware that we you write a substring, you can navigate throgth the options with down arrow key, but you can comeback to the input with the ip arrow key</Typography></li>
       </ol>
 
-      <InputSelect optionsList={allCountries} excludedOptions={list} onAdd={onAddHandler} textFieldProps={{
-        variant: 'outlined'
+      <InputSelect optionsList={allCountries} excludedOptions={selected} onAdd={onAddHandler} textFieldProps={{
+        variant: "outlined"
       }} />
-      {list.map(items => <div key={items.text}>{items.text}</div>)}
+      {
+        selected.map(item =>
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <img alt="Flag" src={item.flag} style={{ width: '24px', height: '24px', margin: '5px' }} />
+            <Typography
+              variant="body1"
+              key={item.text}
+            >
+              {`Country: ${item.text} |  Population: ${item.population}`}
+            </Typography>
+          </div>
+        )}
     </div>
   );
 }
