5ac51824caa51d98a4be387d9bdcaedf4032fe39
Adrian <adrianfdez469@gmail.com>
2021-01-28 02:30:12 -0500

    Adding github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import InputSelect from 'react-material-selectable-inputtext';
 
-function App() {
+const App = () => {
+  const [allCountries, setAllCountries] = React.useState([]);
+  const [list, setList] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      const countriesResp = await fetch('https://restcountries.eu/rest/v2/all');
+      if (countriesResp.ok) {
+        const countries = await countriesResp.json();
+        const mappedCountries = countries.map(c => {
+          return { id: c.name, text: c.name, population: c.population }
+        });
+        setAllCountries(mappedCountries);
+      }
+    })()
+  }, []);
+
+  const onAddHandler = (item) => {
+    setList([...list, item]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '20px' }}>
+      <InputSelect optionsList={allCountries} excludedOptions={list} onAdd={onAddHandler} textFieldProps={{
+        variant: 'outlined'
+      }} />
+      {list.map(items => <div key={items.text}>{items.text}</div>)}
     </div>
   );
 }
