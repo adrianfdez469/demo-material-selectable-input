851a7d166d6589c39fe29c2ef7ff8200dbecca88
Adrian <adrianfdez469@gmail.com>
2021-01-28 03:02:02 -0500

    Adding user help

--- src/App.js
+++ src/App.js
@@ -22,7 +22,17 @@ const App = () => {
   }
 
   return (
-    <div style={{ margin: '20px' }}>
+    <div style={{ margin: '20px', fontSize: '20px' }}>
+      <h2>To see what the component can do, follow the steps.</h2>
+      <ol>
+        <li>Focus the component and white an "A" letter, it will pop up all the countries that begin with "A"</li>
+        <li>Press the arrow down key from your keyboard, and choose "Albania", then press "Enter"</li>
+        <li>Again write an "A" letter, you'll see that "Albania" is no longer available. This time with your mouse select other country option and press "Enter" or press the plus button</li>
+        <li>Now write anther country name without selecting it from the list and added, you can see after that the country is no logner available on the list</li>
+        <li>Also you can write anything that is not on the list, and added as well</li>
+        <li>Be aware that we you write a substring, you can navigate throgth the options with down arrow key, but you can comeback to the input with the ip arrow key</li>
+      </ol>
+
       <InputSelect optionsList={allCountries} excludedOptions={list} onAdd={onAddHandler} textFieldProps={{
         variant: 'outlined'
       }} />
